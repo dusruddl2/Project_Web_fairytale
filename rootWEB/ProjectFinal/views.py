7c139e4e173e3c9d68959a451838f0e00de64c45
choiyewon <choiyewon1997@gmail.com>
2023-09-02 15:01:12 +0900

    final commit

--- rootWEB/ProjectFinal/views.py
+++ rootWEB/ProjectFinal/views.py
@@ -40,10 +40,17 @@ def func_Fairytale(request):
     }
 
     context['ask_str'] = make_question(context)
-    context['answer_chatgpt'] = openapi_chatgpt(context['ask_str'])
-    # context['answer_chatgpt'] ="test_text"
+    # context['answer_chatgpt'] = openapi_chatgpt(context['ask_str'])
+    context['result_chatgpt'] = eval(openapi_chatgpt(context['ask_str']))
+    context['len_chap'] = len(context['result_chatgpt'])
 
-    print(context)
+    context['result_title'] =context['result_chatgpt']['chapter1']['chapter_title']
+    context['result_content'] = make_story(context['result_chatgpt'], context['len_chap'])
+
+    print(context['result_chatgpt'])
+    print(type(context['result_chatgpt']))
+    print(len(context['result_chatgpt']))
+
     return render(request, 'ProjectFinal/fairytale.html', context)
 
 
@@ -70,11 +77,35 @@ def openapi_delle(description, img_cnt , img_size):
 
 
 def make_question(input_info):
-    result_str = f"Can you make a story in the form of 'dictionary'(python) using the following conditions by {input_info['detail_length']} chapters? Each chapter should include"
+    result_str = f"Can you make a story in the form of 'JSON' using the following conditions by {input_info['detail_length']} chapters? Each chapter should include"
     result_str += f"(1) ‘chapter_number’ (2) ‘chapter_title‘ (3) ‘chapter_content’ (at least 5 sentences) (4) ‘chapter_atmosphere’ (5) ‘chapter_background’"
     result_str += f"1. Characters 1-1. Main character 1-1-1. name: {input_info['character_name']} 1-1-2. gender: {input_info['character_gender']} 1-1-3. characteristics: {input_info['character_personality']}"
     #     result_str += f"1-2. Sub character 1-2-1. name: {} 1-2-2. gender: {} 1-2-3. characteristics: {}"
     result_str += f"2. Target audience 2-1. age: {input_info['listener_age']} 2-2. gender: {input_info['listener_gender']}"
     result_str += f"3. Story 3-1. keyword: {input_info['detail_keyword']} 3-2. atmosphere: {input_info['detail_mood']} 3-3. the number of chapters: {input_info['detail_length']}"
+
+    return result_str
+
+def make_question_kor(input_info):
+    result_str = f"나는 {input_info['listener_age']}살 {input_info['listener_gender']} 아이를 위해 {input_info['detail_keyword']}가 주제인 동화를 제작하려 해."
+    result_str += f"주인공 이름은 {input_info['character_name']}이며 성별은 {input_info['character_gender']}이고 {input_info['character_personality']}으면 좋겠어."
+    result_str += f"결과적으로 나는 그 아이를 위해 {input_info['detail_mood']}느낌나는 동화를 제작하려 해. 동화의 길이는 총 {input_info['detail_length']} 챕터로 구성해줘. "
+    result_str += f"동화의 구성은 챕터 순차번호인 'chapter_idx', 챕터 타이틀인 'chapter_title' , 챕터 내용인 'chapter_content',"
+    result_str += f"각 챕터의 분위기를 나타내는 'chapter_mood',  각 챕터가 발생한 장소를 설명한 'chapter_background'로 나타내줘."
+    result_str += f"'chapter_title'은 1개의 문장, 'chapter_content'는 최소 5줄의 문장으로 구성해줬으면 해. "
+    result_str += f"마지막으로 'character'이라는 변수에 주인공 이름인 'name'과 주인공 생김새인 'face'로 추가해줘."
+    result_str += f"또 주인공과 같이 나오는 조연들은 'sub_character'이라는 변수에 각 조연들의 이름을'sub_name', "
+    result_str += f"조연들의 생김새는 'sub_face'에 넣어줘. "
+    result_str += f"최종적으로 dictionary 형식의 데이터만 결과값으로 나에게 줘."
+
+    return result_str
+
+def make_story(fairytale_info, fairytale_len):
+    result_str = ""
+
+    for i in range(1, len(fairytale_len)):
+        chapter_type = f"chapter{i}"
+        result_str += f"{fairytale_info[chapter_type]['chapter_content']}"
+        result_str += "\n"
 
     return result_str
